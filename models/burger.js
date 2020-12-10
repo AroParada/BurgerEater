b83fd34be73e6be832cc3e5a20f553c3c99ef999
harold parada <haroldparada20@gmail.com>
2020-12-10 16:00:43 -0500

    Orm call functions done

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,20 @@
 var orm = require("../config/orm.js");
+
+var burger = {
+    all: (cb) => {
+      orm.all("burgers", (res) => cb(res));
+    },
+    // The variables cols and vals are arrays.
+    create: (cols, vals, cb) => {
+      orm.create("burgers", cols, vals, (res) => cb(res));
+    },
+    update: (colToUpdate, valToUpdate, id, cb) => {
+      orm.update("burgers", colToUpdate, valToUpdate, id, cb, (res) => cb(res));
+    },
+    delete: (colInput, id, cb) => {
+      orm.delete("burgers", colInput, id, (res) =>  cb(res));
+    }
+  };
+  
+  // Exports the database functions for the controller .
+  module.exports = burger;
